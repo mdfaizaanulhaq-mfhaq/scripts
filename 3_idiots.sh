0e0553bbba38b2f43062a1d441a397cdf33da855
Ubuntu <ubuntu@ip-172-31-47-162.eu-west-1.compute.internal>
2026-04-14 07:52:29 +0000

    3_idiots.sh script

---
+++ 3_idiots.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+hero="rancho"
+villain="virus"
+
+echo "3 idiots ka hero hai" $hero
+
+echo "3 idiots ka vilain hai" #villain
+
+
+echo "ypur user is" $USER
+
+read -p "rachu ka pura naam kya tha" fullname
+
+echo "ranchu ka pura naam kya tha?" $fullname
